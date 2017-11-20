2332a50fced18a0747cef31f1b02e42ccfe02868
WilliePart2 <anarhistishche@gmail.com>
2017-11-20 15:58:32 +0200

    Добавил слити для главной страницы и страницы входаэ

--- view/Index.php
+++ view/Index.php
@@ -2,39 +2,36 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title></title>
-    <style>
-        /*Это все перенести в файл стилей*/
-        .top-menu {
-            height: 50px;
-            position: relative;
-        }
-        .top-menu_label{
-            display: inline-block;
-            text-align: left;
-            font-size: 24px;
-        }
-        .sign-btn{
-            display: inline-block;
-            position: absolute;
-            right: 0;
-        }
-        .small{
-            font-size: 16px;
-        }
-        a {
-            text-decoration: none;
-        }
-    </style>
+    <title>Willie-miner</title>
+    <link type="text/css" rel="stylesheet" href="<?php ROOT ?>/templates/css/ResetCSS.css">
+    <link type="text/css" rel="stylesheet" href="<?php ROOT ?>/templates/css/indexStyle.css">
     <script></script>
 </head>
 <body>
 <header class="top-menu">
-    <h1 class="top-menu_label">Willie-Miner.<small class="small">net</small></h1>
+    <h1 class="top-menu_label"><span class="open_php">?php echo </span>Willie-Miner.<small class="small">net</small><span class="close_php">; ?</span></h1>
     <div class="sign-btn">
-        <a href="signin/"><button>Вход</button></a>
-        <a href="signup/"><button>Регистрация</button></a>
+        <a href="signin/"><button class="btn btn_signin">Вход</button></a>
+        <a href="signup/"><button class="btn btn_signout">Регистрация</button></a>
     </div>
 </header>
+<main>
+    <!--Тут будет будет блок для js вывода меню и демонстрационный майнер   -->
+    <div class="main-wrapper">
+        <div class="main_miner">
+            <!-- Тут будет демонстрационный майнер -->
+        </div>
+        <div class="main_form">
+            <!-- Сюда будет выезжать форма для входа\регистрации -->
+        </div>
+    </div>
+</main>
+<div class="border"><!--Пустой блок для ограничивания--></div>
+<footer class="footer">
+    <!--Тут будет подвал сайта с копирайтами-->
+    <div class="footer_item"></div>
+    <div class="footer_item"></div>
+    <div class="footer_item"></div>
+</footer>
 </body>
 </html>
--- view/Registration.php
+++ view/Registration.php
@@ -3,29 +3,34 @@
 <head>
     <meta charset="utf-8">
     <title>Регистрация нового пользователя</title>
+    <link type="text/css" rel="stylesheet" href="<?php ROOT ?>/templates/css/ResetCSS.css">
+    <link type="text/css" rel="stylesheet" href="<?php ROOT ?>/templates/css/registrationStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
 </head>
 <body>
-<h1>Регистрация нового пользователя</h1>
-<form action="registration/" method="POST">
-    <div class="form_group">
-        <label class="form-label" for="login">Логин</label>
-        <input class="form-input" type="text" name="login" placeholder="Введите ваш логин" id="login">
-    </div>
-    <div class="form_group">
-        <label class="form-label" for="email">Email</label>
-        <input class="form-input" type="email" name="email" placeholder="Введите ваш email" id="email">
-    </div>
-    <div class="form_group">
-        <label class="form-label" for="password">Пароль</label>
-        <input class="form-input" type="password" name="password" placeholder="Введите ваш пароль" id="password">
-    </div>
-    <div class="form_group">
-        <label class="form-label" id="repeat_password">Повторие пароль</label>
-        <input class="form-input" type="password" name="repeat_password" placeholder="Повторите пароль" id="repeat_password">
-    </div>
-    <div class="form-group">
-        <button class="btn-submit" type="submit" name="signUp">Регистрация</button>
-    </div>
-</form>
+<div class="wrapper">
+    <h1 class="header">Регистрация</h1>
+    <form class="form" action="registration/" method="POST">
+        <div class="form_group">
+            <label class="form_label" for="login">Логин</label>
+            <input class="form_input" type="text" name="login" id="login">
+        </div>
+        <div class="form_group">
+            <label class="form_label" for="email">Email</label>
+            <input class="form_input" type="email" name="email" id="email">
+        </div>
+        <div class="form_group">
+            <label class="form_label" for="password">Пароль</label>
+            <input class="form_input" type="password" name="password" id="password">
+        </div>
+        <div class="form_group">
+            <label class="form_label" id="repeat_password">Повторие пароль</label>
+            <input class="form_input" type="password" name="repeat_password" id="repeat_password">
+        </div>
+        <div class="form-group">
+            <button class="btn_submit" type="submit" name="signUp">Регистрация</button>
+        </div>
+    </form>
+</div>
 </body>
 </html>
